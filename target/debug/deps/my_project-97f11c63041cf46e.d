8638cae2afb872fedc1c466bfd9c56242f13ee5c
Ken Zhou <74420508+notken12@users.noreply.github.com>
2022-03-28 14:53:36 +0000

    Initial commit

---
+++ target/debug/deps/my_project-97f11c63041cf46e.d
@@ -0,0 +1,5 @@
+/home/runner/Rust/target/debug/deps/my_project-97f11c63041cf46e.rmeta: src/main.rs
+
+/home/runner/Rust/target/debug/deps/my_project-97f11c63041cf46e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/my_project-d99c75aada7cfe1a.d
@@ -0,0 +1,5 @@
+/home/runner/Rust/target/debug/deps/my_project-d99c75aada7cfe1a.rmeta: src/main.rs
+
+/home/runner/Rust/target/debug/deps/my_project-d99c75aada7cfe1a.d: src/main.rs
+
+src/main.rs:
