c0a2bb1f07bcc420ef130db894e5c327eb1135a3
notken12 <kendotzhou@gmail.com>
2022-03-28 17:56:27 -0400

    remove target

--- target/debug/deps/my_project-97f11c63041cf46e.d
+++
@@ -1,5 +0,0 @@
-/home/runner/Rust/target/debug/deps/my_project-97f11c63041cf46e.rmeta: src/main.rs
-
-/home/runner/Rust/target/debug/deps/my_project-97f11c63041cf46e.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/my_project-d99c75aada7cfe1a.d
+++
@@ -1,5 +0,0 @@
-/home/runner/Rust/target/debug/deps/my_project-d99c75aada7cfe1a.rmeta: src/main.rs
-
-/home/runner/Rust/target/debug/deps/my_project-d99c75aada7cfe1a.d: src/main.rs
-
-src/main.rs:
