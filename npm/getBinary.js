0fcd6ee40cc92c2a9a327e93eb6ed86cfb0e3f13
notken12 <kendotzhou@gmail.com>
2022-03-30 22:00:14 -0400

    make npm

---
+++ npm/getBinary.js
@@ -0,0 +1,25 @@
+const { Binary } = require("binary-install");
+
+const os = require("os");
+
+function getPlatform() {
+  const type = os.type();
+  const arch = os.arch();
+
+  if (type === "Windows_NT" && arch === "x64") return "win64";
+  // Doesn't support win32 yet. TODO: add build task for it.
+  // if (type === 'Windows_NT') return 'win32';
+  if (type === "Linux" && arch === "x64") return "linux";
+  if (type === "Darwin" && arch === "x64") return "macos";
+
+  throw new Error(`Unsupported platform: ${type} ${arch}`);
+}
+
+function getBinary() {
+  const version = require("../package.json").version;
+  const url = `https://github.com/notken12/licensesnip/releases/download/v${version}/licensesnip-win64.tar.gz`;
+  const name = "licensesnip";
+  return new Binary(url, { name });
+}
+
+module.exports = getBinary;
